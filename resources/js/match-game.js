1a09707c883d45d63321389b3cd2e2e9fc4dabf4
Bubbles2016 <hsein.barada@gmail.com>
2017-06-30 12:06:06 +0300

    Adding generateCardValues function

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -10,7 +10,33 @@ var MatchGame = {};
  */
 
 MatchGame.generateCardValues = function () {
-
+  var in_order_card_values = new Array();
+  for (var i = 0; i < 8; i++) {
+    in_order_card_values.push(i+1);
+    in_order_card_values.push(i+1);
+  }
+  var ramdomly_ordered_card_values = new Array();
+  var i = 0;
+  /*Following is one approach on how to generate a random number between 0 and 16 using Math.random()*/
+  var min = 0;
+  var max = 16;
+  /*The Math.ceil() function returns the smallest integer greater than or equal to a given number.*/
+  min = Math.ceil(min);
+  /*The Math.floor() function returns the largest integer less than or equal to a given number.*/
+  max = Math.floor(max);
+  var ordered_array_length = in_order_card_values.length;
+  while (i < ordered_array_length) {
+    /*The following formula is taken from MDN documentation to generate a random number between min and max (excluded)*/
+    var j = Math.floor(Math.random() * (max - min)) + min;
+    /*While the value at the index generated is not defined, generate another index*/
+    while (in_order_card_values[j] == null) {
+      j = Math.floor(Math.random() * (max - min)) + min;
+    }
+    ramdomly_ordered_card_values.push(in_order_card_values[j]);
+    in_order_card_values.splice(j, 1);
+    i = i + 1;
+  }
+  return ramdomly_ordered_card_values;
 };
 
 /*
